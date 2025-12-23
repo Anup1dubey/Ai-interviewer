c7bf417dadd4a1fd8d484a5c0299d61aa8b02ec1
Anup dubey <anup1dubey@gmail.com>
2025-12-23 13:20:40 +0530

    saving user info in supabase changes done

--- ai-interviewer/app/auth/page.jsx
+++ ai-interviewer/app/auth/page.jsx
@@ -1,9 +1,24 @@
+"use client"
 import React from 'react'
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { supabase } from '@/services/supabaseClient';
 
 
-function login() {
+function Login() {
+
+    const signInWithGoogle= async ()=>{
+        const {error}=await supabase.auth.signInWithOAuth({
+            provider:'google'
+        })
+
+        if(error){
+            console.error('Error:',error.message)
+        }
+    }
+
+
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <div className='flex flex-col items-center'>
@@ -16,13 +31,14 @@ function login() {
             width={600}
             height={400}
             className='w-[400px] h-[250px] border rounded-2xl'/>
-            <h2 className='text-2x font bold text-center mt-5'>Welcome to a-interviewer</h2>
+            <h2 className='text-2xL font-bold text-center mt-5'>Welcome to a-interviewer</h2>
             <p className='text-gray-500 text-center'>Sign In with Google Authentication</p>
-            <Button className='mt-7 w-full'> Login with Google</Button>
+            <Button className='mt-7 w-full'
+             onClick={signInWithGoogle}> Login with Google</Button>
         </div>
       </div>
     </div>
   )
 }
 
-export default login
+export default Login
